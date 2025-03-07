f3c725b82e5a2344fa19d02cbc50d92653c4a36a
gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2025-03-07 09:17:31 +0100

    Añadir a properties y test GetMapping

--- catalogo/src/main/java/com/example/demo/CatalogoApplication.java
+++ catalogo/src/main/java/com/example/demo/CatalogoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class CatalogoApplication {
+@RestController
+public class CatalogoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hi there!";
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Applicación arrancada");
 	}
 
 }
