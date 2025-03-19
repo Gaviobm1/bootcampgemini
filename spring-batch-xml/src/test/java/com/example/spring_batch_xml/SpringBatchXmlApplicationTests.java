db55b460cc1c7e5a7c28d1b721e3240b249b1373
gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2025-03-19 10:42:26 +0100

    Añadir xml implementacion

---
+++ spring-batch-xml/src/test/java/com/example/spring_batch_xml/SpringBatchXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
