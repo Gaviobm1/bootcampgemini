0d55d81a180d16824aa5839e0db0698350dc03d9
gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2025-03-26 13:57:01 +0100

    Añadir demo de JS

---
+++ frontend/demo.js
@@ -0,0 +1,92 @@
+const average = (...args) => {
+  let rslt = 0;
+  if (!args.length) {
+    return rslt;
+  }
+  for (let num of args) {
+    rslt += num;
+  }
+  return rslt / args.length;
+};
+
+const mates = {
+  PI: 3.141592654,
+  average: (...args) => {
+    let rslt = 0;
+    if (!args.length) {
+      return rslt;
+    }
+    for (let num of args) {
+      rslt += num;
+    }
+    return rslt / args.length;
+  },
+  add: (...args) => {
+    let rslt = 0;
+    for (let num of args) {
+      rslt += num;
+    }
+    return rslt;
+  },
+  circleArea: function (r) {
+    return this.PI * r ** 2;
+  },
+};
+
+function MiClase(elId, elNombre) {
+  const obj = this;
+  this.id = elId;
+  this.nombre = elNombre;
+  this.muestraId = function () {
+    alert("El ID del objeto es " + this.id);
+  };
+  this.ponNombre = (nom) => {
+    obj.nombre = nom.toUpperCase();
+  };
+}
+
+function createCounter() {
+  let count = 0;
+
+  return {
+    increment: function () {
+      count++;
+    },
+    decrement: function () {
+      count--;
+    },
+    getCount: function () {
+      return count;
+    },
+    resetCount: function () {
+      count = 0;
+    },
+  };
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+  set width(width) {
+    this._width = width;
+  }
+  get width() {
+    return this._width;
+  }
+  set height(height) {
+    this._height = height;
+  }
+  get height() {
+    return this._height;
+  }
+  get area() {
+    return this._width * this._height;
+  }
+}
+
+const rect = new Rectangle(7, 4);
+console.log(rect.area);
+rect.height = 5;
+console.log(rect.area);
