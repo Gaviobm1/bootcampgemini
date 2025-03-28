709cd344cab2949930996f5016f20e9bea0fd064
gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2025-03-28 13:37:41 +0100

    Implementar subscriber

--- demo-ng/src/app/demos/demos.component.ts
+++ demo-ng/src/app/demos/demos.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NotificationService } from '@my/common-services';
+import { Unsubscribable } from 'rxjs';
+import { NotificationType } from '@my/common-services';
 
 @Component({
   selector: 'app-demos',
   templateUrl: './demos.component.html',
   styleUrl: './demos.component.css',
 })
-export class DemosComponent {
+export class DemosComponent implements OnInit, OnDestroy {
+  private suscriptor: Unsubscribable | undefined;
+
   constructor(public vm: NotificationService) {}
+
+  ngOnInit(): void {
+    this.suscriptor = this.vm.Notificacion.subscribe((n) => {
+      if (n.Type !== NotificationType.error) {
+        return;
+      }
+      window.alert(`Suscripción: ${n.Message}`);
+      this.vm.remove(this.vm.Listado.length - 1);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.suscriptor) {
+      this.suscriptor.unsubscribe();
+    }
+  }
 }
